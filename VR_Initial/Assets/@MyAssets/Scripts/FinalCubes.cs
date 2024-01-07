b71a9e59d9686c010c4bc327e4f1db58a5d8e837
Sara Bolaños <sara.bolanos@opendeusto.es>
2024-01-07 02:10:19 +0100

    sockets

--- VR_Initial/Assets/@MyAssets/Scripts/FinalCubes.cs
+++ VR_Initial/Assets/@MyAssets/Scripts/FinalCubes.cs
@@ -11,15 +11,7 @@ public class FinalCubes : MonoBehaviour
     public GameObject door_r;
 
     // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
 
     public void SocketCheck()
     {
