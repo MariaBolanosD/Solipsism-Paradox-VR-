c08f0a121dee5b7d43254839aa43638da78d9516
Sara Bolaños <sara.bolanos@opendeusto.es>
2024-01-07 02:55:50 +0100

    final door

--- VR_Initial/Assets/@MyAssets/SaritaOBJ/finaldoor.cs
+++ VR_Initial/Assets/@MyAssets/SaritaOBJ/finaldoor.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class finaldoor : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public GameObject parent;
+    public XRSocketInteractor socket;
+    public GameObject door_l;
+    public GameObject door_r;
+    public void Green()
     {
-        
+        IXRSelectInteractable objName = socket.GetOldestInteractableSelected();
+
+       
+        if (objName.transform.name == ("Cube_VERDE"))
+        {
+            door_l.SetActive(false);
+            door_r.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Red()
     {
-        
+        IXRSelectInteractable objName = socket.GetOldestInteractableSelected();
+        if (objName.transform.name == ("Cube_ROJO"))
+        {
+            door_l.SetActive(false);
+            door_r.SetActive(false);
+        }
+
+    }
+
+    public void Blue()
+    {
+        IXRSelectInteractable objName = socket.GetOldestInteractableSelected();
+        if (objName.transform.name == ("Cube_AZUL"))
+        {
+            door_l.SetActive(false);
+            door_r.SetActive(false);
+        }
     }
 }
--- VR_Initial/Assets/@MyAssets/Scripts/FinalCubes.cs
+++ VR_Initial/Assets/@MyAssets/Scripts/FinalCubes.cs
@@ -11,25 +11,25 @@ public class FinalCubes : MonoBehaviour
     public GameObject door_r;
 
     // Start is called before the first frame update
- 
+
 
     public void SocketCheck()
     {
         IXRSelectInteractable objName = socket.GetOldestInteractableSelected();
-        if(this.gameObject.name.Equals("Cube_BLUE") && objName.Equals("Cube_AZUL"))
+        if (this.gameObject.name.Equals("Cube_BLUE") && (objName.transform.name == ("Cube_AZUL")))
         {
             parent.GetComponent<ParentCube>().Cube_blue = true;
-            if(parent.GetComponent<ParentCube>().Cube_green && parent.GetComponent<ParentCube>().Cube_blue && parent.GetComponent<ParentCube>().Cube_red)
+            if (parent.GetComponent<ParentCube>().Cube_green && parent.GetComponent<ParentCube>().Cube_blue && parent.GetComponent<ParentCube>().Cube_red)
             {
                 door_l.SetActive(false);
                 door_r.SetActive(false);
             }
         }
-        else
+        else if (this.gameObject.name.Equals("Cube_BLUE") && (objName.transform.name != ("Cube_AZUL")))
         {
             parent.GetComponent<ParentCube>().Cube_blue = false;
         }
-        if (this.gameObject.name.Equals("Cube_RED") && objName.Equals("Cube_ROJO"))
+        if (this.gameObject.name.Equals("Cube_RED") && (objName.transform.name == ("Cube_ROJO")))
         {
             parent.GetComponent<ParentCube>().Cube_red = true;
             if (parent.GetComponent<ParentCube>().Cube_green && parent.GetComponent<ParentCube>().Cube_blue && parent.GetComponent<ParentCube>().Cube_red)
@@ -38,22 +38,25 @@ public class FinalCubes : MonoBehaviour
                 door_r.SetActive(false);
             }
         }
-        else
+        else if (this.gameObject.name.Equals("Cube_RED") && (objName.transform.name != ("Cube_ROJO")))
         {
-            parent.GetComponent<ParentCube>().Cube_red = false;
-        }
-        if (this.gameObject.name.Equals("Cube_GREEN") && objName.Equals("Cube_VERDE"))
-        {
-            parent.GetComponent<ParentCube>().Cube_green = true;
-            if (parent.GetComponent<ParentCube>().Cube_green && parent.GetComponent<ParentCube>().Cube_blue && parent.GetComponent<ParentCube>().Cube_red)
             {
-                door_l.SetActive(false);
-                door_r.SetActive(false);
+                parent.GetComponent<ParentCube>().Cube_red = false;
             }
-        }
-        else
-        {
-            parent.GetComponent<ParentCube>().Cube_green = false;
+            if (this.gameObject.name.Equals("Cube_GREEN") && (objName.transform.name == ("Cube_VERDE")))
+            {
+                parent.GetComponent<ParentCube>().Cube_green = true;
+                if (parent.GetComponent<ParentCube>().Cube_green && parent.GetComponent<ParentCube>().Cube_blue && parent.GetComponent<ParentCube>().Cube_red)
+                {
+                    door_l.SetActive(false);
+                    door_r.SetActive(false);
+                }
+            }
+            else if (this.gameObject.name.Equals("Cube_GREEN") && (objName.transform.name != ("Cube_VERDE")))
+
+            {
+                parent.GetComponent<ParentCube>().Cube_green = false;
+            }
         }
     }
 
