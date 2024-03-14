52263ca1bfdbc1fdbc8e858e838a6e26b2fce68e
SaraBolanos <145749013+SaraBolanos@users.noreply.github.com>
2024-03-14 08:09:20 +0100

    nivel y score

---
+++ VR_Initial/Assets/@MyAssets/Scripts/ScoreUpdater.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUpdater : MonoBehaviour
+{
+    [SerializeField] GameObject TargetLeft;
+    [SerializeField] GameObject TargetRight;
+
+
+    private Vector3 distance_Player = new Vector3(150,150,150);
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+
+
+    }
+}
