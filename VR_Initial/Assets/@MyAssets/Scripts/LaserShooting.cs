5517e9aab99aefde58a2bb124042de7aac9d102d
MariaBolanosD <maria.bolanos@opendeusto.es>
2024-03-14 09:40:33 +0100

    dispara al forward del objeto

--- VR_Initial/Assets/@MyAssets/Scripts/LaserShooting.cs
+++ VR_Initial/Assets/@MyAssets/Scripts/LaserShooting.cs
@@ -1,14 +1,18 @@
-using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
 
 public class LaserShooting : MonoBehaviour
 {
 
     public int gunDamage = 1;                                            // Set the number of hitpoints that this gun will take away from shot objects with a health script
     public float fireRate = 0.25f;                                        // Number in seconds which controls how often the player can fire
-    public float weaponRange = 50f;                                        // Distance in Unity units over which the player can fire
+    public float weaponRange = 500f;                                        // Distance in Unity units over which the player can fire
     public float hitForce = 100f;                                        // Amount of force which will be added to objects with a rigidbody shot by the player
     public Transform gunEnd;                                            // Holds a reference to the gun end object, marking the muzzle location of the gun
+
+ 
 
     private Camera fpsCam;                                                // Holds a reference to the first person camera
     private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);    // WaitForSeconds object used by our ShotEffect coroutine, determines time laser line will remain visible
@@ -41,8 +45,10 @@ public class LaserShooting : MonoBehaviour
             // Start our ShotEffect coroutine to turn our laser line on and off
             StartCoroutine(ShotEffect());
 
+
+
             // Create a vector at the center of our camera's viewport
-            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+            Vector3 rayOrigin = this.transform.position;// fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
 
             // Declare a raycast hit to store information about what our raycast has hit
             RaycastHit hit;
@@ -51,7 +57,9 @@ public class LaserShooting : MonoBehaviour
             laserLine.SetPosition(0, gunEnd.position);
 
             // Check if our raycast has hit anything
-            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+            Ray ray = new Ray(this.transform.position, this.transform.forward);
+            //if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+            if (Physics.Raycast(ray, out hit, weaponRange))
             {
                 // Set the end position for our laser line 
                 laserLine.SetPosition(1, hit.point);
@@ -76,7 +84,8 @@ public class LaserShooting : MonoBehaviour
             else
             {
                 // If we did not hit anything, set the end of the line to a position directly in front of the camera at the distance of weaponRange
-                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+                //laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+                laserLine.SetPosition(1, rayOrigin + (this.transform.forward * weaponRange));
             }
         }
     }
--- VR_Initial/Assets/@MyAssets/Scripts/RayViewerComplete.cs
+++ VR_Initial/Assets/@MyAssets/Scripts/RayViewerComplete.cs
@@ -19,9 +19,12 @@ public class RayViewerComplete : MonoBehaviour
     void Update()
     {
         // Create a vector at the center of our camera's viewport
-        Vector3 lineOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+        //Vector3 lineOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+        Vector3 lineOrigin = this.transform.position;
+
 
         // Draw a line in the Scene View  from the point lineOrigin in the direction of fpsCam.transform.forward * weaponRange, using the color green
-        Debug.DrawRay(lineOrigin, fpsCam.transform.forward * weaponRange, Color.green);
+        //Debug.DrawRay(lineOrigin, fpsCam.transform.forward * weaponRange, Color.green);
+        Debug.DrawRay(lineOrigin, this.transform.forward * weaponRange, Color.green);
     }
 }
