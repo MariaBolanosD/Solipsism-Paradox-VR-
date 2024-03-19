f764488e2196e1907335f924947a6b10c03a3ac6
SaraBolanos <145749013+SaraBolanos@users.noreply.github.com>
2024-03-19 11:11:06 +0100

    funciona randomizafor

--- VR_Initial/Assets/@MyAssets/Scripts/ScoreUpdater.cs
+++ VR_Initial/Assets/@MyAssets/Scripts/ScoreUpdater.cs
@@ -15,15 +15,49 @@ public class ScoreUpdater : MonoBehaviour
     }
 
 
-    // Update is called once per frame
-    private void OnCollisionEnter(Collision collision)
+    void Update()
     {
-        if(collision.collider.CompareTag(tagger))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Vector3 rndPosWithin;
-            rndPosWithin = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-            rndPosWithin = transform.TransformPoint(rndPosWithin * 0.5f);
-            Target.transform.position = rndPosWithin;
+             TeleportInsideTaggedArea();
         }
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag(tagger))
+        {
+            TeleportInsideTaggedArea();
+        }
+    }
+
+    void TeleportInsideTaggedArea()
+    {
+        GameObject taggedObject = GameObject.FindGameObjectWithTag(tagger);
+        if (taggedObject != null)
+        {
+            Collider collider = taggedObject.GetComponent<Collider>();
+            if (collider != null)
+            {
+                Vector3 center = collider.bounds.center;
+                Vector3 size = collider.bounds.size;
+
+                Vector3 rndPosInside;
+                rndPosInside = new Vector3(Random.Range(-size.x / 2f, size.x / 2f),
+                                           Random.Range(-size.y / 2f, size.y / 2f),
+                                           Random.Range(-size.z / 2f, size.z / 2f));
+                rndPosInside += center;
+                Target.transform.position = rndPosInside;
+            }
+            else
+            {
+                Debug.LogError("No collider found on object with tag: " + tagger);
+            }
+        }
+        else
+        {
+            Debug.LogError("No object found with tag: " + tagger);
+        }
+    }
+
 }
